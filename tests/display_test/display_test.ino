e9728dc499ac7d8733ab1a73806a8c58d10b22a4
Janis Zarans <janis.zarans@gmail.com>
2026-03-21 22:12:30 +0200

    Add working OLED display test

--- tests/display_test/display_test.ino
+++ tests/display_test/display_test.ino
@@ -1,2 +1,30 @@
-// Display test
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Wire.begin();  // UNO Q I2C on A4/A5
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (true) {
+    }
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("OLED test OK");
+  display.println("Arduino UNO Q");
+  display.display();
+}
+
+void loop() {
+}// Display test
 // This file will contain code to test the display
